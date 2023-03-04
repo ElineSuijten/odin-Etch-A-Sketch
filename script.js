3ecd9f4b3fd6934391935b208efc1e2eccbfeea3
Eline Suijten <elinesuijten@live.nl>
2023-03-04 11:45:49 +0100

    Add on mouseover function

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ function createGrid() {
         const cell = document.createElement('div');
         cell.classList.add('gridCell');
         grid.appendChild(cell);
+        cell.addEventListener('mouseover', function mouseOver() {
+            cell.classList.add('gridCellHover');
+        });
     }
 }
+
+
+
 
 createGrid();
